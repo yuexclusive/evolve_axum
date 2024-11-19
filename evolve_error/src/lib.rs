d74b41705fcb81b7ed5763894781c9c8183e8643
yu <liuyu@ghgame.cn>
2024-11-19 18:16:34 +0800

    refactor

--- evolve_error/src/lib.rs
+++ evolve_error/src/lib.rs
@@ -2,7 +2,7 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 use axum_extra::typed_header::TypedHeaderRejection;
 use jsonwebtoken::errors::Error as JwtError;
@@ -28,6 +28,9 @@ impl Display for AuthError {
 
 #[derive(Debug, Error)]
 pub enum AppError {
+    #[error(transparent)]
+    Wps(#[from] ErrorResp),
+
     #[error("internal error: {}",.msg)]
     Internal { msg: String },
 
@@ -78,6 +81,9 @@ pub enum AppError {
 
     #[error(transparent)]
     InvalidHeaderValue(#[from] http::header::InvalidHeaderValue),
+
+    #[error("other error occurred: {0}")]
+    Other(#[from] anyhow::Error),
 }
 
 impl<T> From<AppError> for AppResult<T> {
@@ -87,20 +93,30 @@ impl<T> From<AppError> for AppResult<T> {
 }
 
 impl ErrorResp {
-    fn new(code: usize, message: &str) -> Self {
+    fn new(code: usize, message: &str, details: Option<String>) -> Self {
         Self {
             code,
-            message: message.to_string(),
+            msg: Some(message.to_string()),
+            detail: details,
         }
     }
 }
 
 // How we want errors responses to be serialized
-#[derive(Serialize, ToSchema)]
+#[derive(Serialize, ToSchema, Deserialize, Debug)]
 pub struct ErrorResp {
-    code: usize,
-    message: String,
+    pub code: usize,
+    pub msg: Option<String>,
+    pub detail: Option<String>,
 }
+
+impl std::fmt::Display for ErrorResp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{}", serde_json::to_string(&self).unwrap()))
+    }
+}
+
+impl std::error::Error for ErrorResp {}
 
 impl IntoResponse for ErrorResp {
     fn into_response(self) -> Response {
@@ -113,69 +129,72 @@ impl IntoResponse for ErrorResp {
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let (status, err_response) = match self {
+            AppError::Wps(err_resp) => (StatusCode::INTERNAL_SERVER_ERROR, err_resp),
+
             AppError::Internal { msg } => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                ErrorResp::new(500100000, &msg),
+                ErrorResp::new(500100000, &msg, None),
             ),
 
-            AppError::Validate { msg } => {
-                (StatusCode::BAD_REQUEST, ErrorResp::new(400100000, &msg))
-            }
+            AppError::Validate { msg } => (
+                StatusCode::BAD_REQUEST,
+                ErrorResp::new(400100000, &msg, None),
+            ),
 
             AppError::Hint { msg } => (
                 StatusCode::from_u16(452).unwrap(),
-                ErrorResp::new(452100000, &msg),
+                ErrorResp::new(452100000, &msg, None),
             ),
 
             AppError::Auth(err) => match err {
                 AuthError::MissingCredentials => (
                     StatusCode::BAD_REQUEST,
-                    ErrorResp::new(400100001, "missing credentials"),
+                    ErrorResp::new(400100001, "missing credentials", None),
                 ),
                 AuthError::WrongCredentials => (
                     StatusCode::UNAUTHORIZED,
-                    ErrorResp::new(401100001, "wrong credentials"),
+                    ErrorResp::new(401100001, "wrong credentials", None),
                 ),
                 AuthError::TokenCreation => (
                     StatusCode::INTERNAL_SERVER_ERROR,
-                    ErrorResp::new(500100001, "token creation failed"),
+                    ErrorResp::new(500100001, "token creation failed", None),
                 ),
             },
             AppError::Jwt(err) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                ErrorResp::new(400100002, &err.to_string()),
+                ErrorResp::new(400100002, &err.to_string(), None),
             ),
             AppError::TypedHeaderRejection(err) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                ErrorResp::new(500100003, &err.to_string()),
+                ErrorResp::new(500100003, &err.to_string(), None),
             ),
             AppError::Redis(err) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                ErrorResp::new(500100004, &err.to_string()),
+                ErrorResp::new(500100004, &err.to_string(), None),
             ),
             AppError::ChronoParse(err) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                ErrorResp::new(500100005, &err.to_string()),
+                ErrorResp::new(500100005, &err.to_string(), None),
             ),
             AppError::Sqlx(err) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                ErrorResp::new(500100006, &err.to_string()),
+                ErrorResp::new(500100006, &err.to_string(), None),
             ),
             AppError::Meilisearch(err) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                ErrorResp::new(500100007, &err.to_string()),
+                ErrorResp::new(500100007, &err.to_string(), None),
             ),
             AppError::SMTP(err) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                ErrorResp::new(500100008, &err.to_string()),
+                ErrorResp::new(500100008, &err.to_string(), None),
             ),
             AppError::Regex(err) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                ErrorResp::new(500100009, &err.to_string()),
+                ErrorResp::new(500100009, &err.to_string(), None),
             ),
             _x => (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                ErrorResp::new(500999999, _x.to_string().as_str()),
+                ErrorResp::new(500999999, _x.to_string().as_str(), None),
             ),
         };
 
