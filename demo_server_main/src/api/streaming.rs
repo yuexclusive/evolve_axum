4a2e465ca156763a1d37a9cf1ebc6e419a76b713
yu <liuyu@ghgame.cn>
2025-03-19 09:58:18 +0800

    init

--- demo_server_main/src/api/streaming.rs
+++ demo_server_main/src/api/streaming.rs
@@ -1,26 +1,30 @@
-use async_stream::stream;
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::response::sse::{Event, Sse};
+use axum_extra::{headers, TypedHeader};
+use futures::stream::{self, Stream};
 use std::time::Duration;
 use tokio_stream::StreamExt;
 
-pub async fn streaming() -> impl IntoResponse {
-    let stream: async_stream::__private::AsyncStream<Result<String, std::io::Error>, _> = stream! {
-        for i in 0..10 {
-            yield Ok(format!("data: {}\n\n", format!("Chunk {}", i)));
-        }
-    };
+pub async fn streaming(
+    TypedHeader(user_agent): TypedHeader<headers::UserAgent>,
+) -> Sse<impl Stream<Item = Result<Event, std::convert::Infallible>>> {
+    println!("`{}` connected", user_agent.as_str());
 
-    let headers = [
-        (axum::http::header::CACHE_CONTROL, "no-cache"),
-        (axum::http::header::CONTENT_TYPE, "text/event-stream"),
-        (axum::http::header::CONNECTION, "keep-alive"),
+    let items = vec![
+        serde_json::json!({"id": 1, "name": "Alice"}),
+        serde_json::json!({"id": 2, "name": "Bob"}),
+        serde_json::json!({"id": 3, "name": "Charlie"}),
     ];
 
-    let throttled_stream = stream.throttle(Duration::from_secs(1));
+    let stream = stream::iter(items)
+        .map(|item| {
+            let data = serde_json::to_string(&item).unwrap();
+            Ok(Event::default().data(data))
+        })
+        .throttle(Duration::from_secs(1)); // send event every second
 
-    (
-        StatusCode::OK,
-        headers,
-        axum::body::Body::from_stream(throttled_stream),
+    Sse::new(stream).keep_alive(
+        axum::response::sse::KeepAlive::new()
+            .interval(Duration::from_secs(1))
+            .text("keep-alive-text"),
     )
 }
